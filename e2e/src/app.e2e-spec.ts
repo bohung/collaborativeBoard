b2e4424f708d7a3594011fd6928988a95228508a
BoHungLiou <monkeytai3@gmail.com>
2021-05-22 18:24:34 +0800

    rename project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('yjsFrontend app is running!');
+    expect(await page.getTitleText()).toEqual('collaborativeBoard app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yjsFrontend'`, () => {
+  it(`should have as title 'collaborativeBoard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('yjsFrontend');
+    expect(app.title).toEqual('collaborativeBoard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('yjsFrontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('collaborativeBoard app is running!');
   });
 });
